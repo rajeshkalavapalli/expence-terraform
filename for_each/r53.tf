9033efc9fcae97552387b906b47123a95dc05a7c
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-07-01 22:19:18 +0530

    route53

--- for_each/r53.tf
+++ for_each/r53.tf
@@ -4,7 +4,7 @@ resource "aws_route53_record" "expense" {
   name    = each.key == "frontend" ? var.domain_name : "${each.key}.${var.domain_name}"
   type    = "A"
   ttl     = 1
-  records = each.key == "frontend" ? [each.value.public_ip] : [each.value.private_ip]
+  records = each.key == "frontend" ? each.value.public_ip : each.value.private_ip
   # if records already exists
   allow_overwrite = true
 }
